cd5c8e63f3d6ec56afa1a1d863030a9c9666219d
jberanova <jana.beranova@student.spsmb.cz>
2022-12-05 10:32:39 +0100

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,6 @@
+const tlacitk01 = document.getElementById("tlacitk01");
+const tlacitk02 = document.getElementById("tlacitk02");
+const tlacitk03 = document.getElementById("tlacitk03");
+const tlacitk04 = document.getElementById("tlacitk04");
+const tlacitk05 = document.getElementById("tlacitk05");
+
